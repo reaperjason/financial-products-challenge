d095b831350a2bf435b714f516ea00de3d5ba11c
reaperjason <carlos.chicaiza2@gmail.com>
2025-08-24 00:46:29 -0500

    feat(router): configure app routing module

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductFormComponent } from './pages/product-form/product-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'new', component: ProductFormComponent },
+  { path: 'edit/:id', component: ProductFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
